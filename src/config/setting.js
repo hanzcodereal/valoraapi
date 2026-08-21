1c05d9a1e959e5e4f60c2c379e2d974975fcaf18
hanzcodereal <hanzzcodee@gmail.com>
2026-08-21 08:25:18 +0700

    Create setting.js

---
+++ src/config/setting.js
@@ -0,0 +1,27 @@
+export const settings = {
+  creator: 'hanzcode',
+  apiKey: 'valora-api',
+  validApiKeys: ['valora-api'],
+  name: 'Valora API',
+  version: '2.0.0',
+  apiUrl: 'valora-api.vercel.app',
+  baseUrl: 'https://valoraapi.vercel.app',
+  description: 'Simple, reliable, and developer-first REST API services built with Next.js and Hono.',
+  icon: '/icon.png',
+  author: 'hanzcode',
+
+  // Support
+  supportUrl: 'https://saweria.co/hanzreally',
+
+  tqto: [
+    {
+      name: 'hanzcode',
+      role: 'Lead Dev & Owner',
+      desc: 'Pembuat & pengembang Valora API',
+      image: '/hanzcode.png',
+      support: 'https://saweria.co/hanzreally'
+    }
+  ]
+}
+
+export default settings
