70e4eac08045e539c906cb1b0c2c6ef8bc834ce2
hanzcodereal <hanzzcodee@gmail.com>
2026-08-21 08:23:35 +0700

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
+import app from './app.js'
+import logger from './utils/logger.ts'
+
+app.get('/static/*', serveStatic({ root: './' }))
+app.get('/docs', serveStatic({ path: './public/docs.html' }))
+app.get('/', serveStatic({ path: './public/index.html' }))
+
+const port = process.env.PORT || 3000
+logger.ready(`Server is running on port ${port}`)
+
+serve({
+    fetch: app.fetch,
+    port
+})
